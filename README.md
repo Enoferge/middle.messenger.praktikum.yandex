3315604d1b23f92344c18dc2f912348a9bb86082
Enoferge <jade.escapist@yandex.ru>
2025-05-18 20:05:51 +0300

    fix correct page on dom-content-loaded, change links, process back button

--- README.md
+++ README.md
@@ -2,13 +2,28 @@
 
 A tiny minimalist messenger with a touch of *kawaii* aesthetics.
 
+## Pages Sprint 1
+- [Sign up page](https://kawaii-messenger.netlify.app/registration)
+- [Sign in page](https://kawaii-messenger.netlify.app/login)
+- [Home page](https://kawaii-messenger.netlify.app/home)
+- [Profile read-only](https://kawaii-messenger.netlify.app/profileRead)
+- [Profile edit](https://kawaii-messenger.netlify.app/profileEdit)
+- [Profile change password](https://kawaii-messenger.netlify.app/profileChangePass)
+- [Profile change avatar](https://kawaii-messenger.netlify.app/profileChangeAvatar)
+- [Profile change avatar error](https://kawaii-messenger.netlify.app/profileChangeAvatarError)
+- [Profile change avatar uploaded](https://kawaii-messenger.netlify.app/profileChangeAvatarUploaded)
+- [Messenger (placeholder for now)](https://kawaii-messenger.netlify.app/messenger)
+- [500 error page](https://kawaii-messenger.netlify.app/500)
+- [400 error page](https://kawaii-messenger.netlify.app/400)
+- [404 error page](https://kawaii-messenger.netlify.app/404)
+
 ## 🛠 Tech Stack
 
 Client:  
 TypeScript, JavaScript
 
 Tools:  
-Vite, PostCSS
+Vite, PostCSS, Netlify
 
 ## 🎨 Design
 
--- src/main.ts
+++ src/main.ts
@@ -6,26 +6,30 @@ import { isPageName } from './utils/isPageName';
 import { registerPartials } from './templates/partials';
 import { registerHelpers } from './templates/helpers';
 import { pages } from './navigation/router';
-import { PAGE_NAMES } from './navigation/constants';
+import { getCurrentPageNameFromPath } from './utils/getCurrentPageFromPath';
 
 registerPartials();
 registerHelpers();
 
 document.addEventListener('click', (e) => {
   const target = e.target as HTMLElement;
-  if (target.tagName === 'A' && target.getAttribute('href')?.startsWith('#')) {
+  if (target.tagName === 'A' && target.getAttribute('href')?.startsWith('/')) {
     e.preventDefault();
     const page = target.getAttribute('href')!.slice(1);
     if (page && isPageName(page) && pages[page]) {
       renderPage(page);
-      history.pushState(null, '', `#${page}`);
+      history.pushState(null, '', `/${page}`);
     } else {
       renderPage('404');
-      history.pushState(null, '', '#404');
+      history.pushState(null, '', '404');
     }
   }
 });
 
 document.addEventListener('DOMContentLoaded', () => {
-  renderPage(PAGE_NAMES.HOME);
+  renderPage(getCurrentPageNameFromPath())
 });
+
+window.addEventListener('popstate', () => {
+  renderPage(getCurrentPageNameFromPath())
+})
