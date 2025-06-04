b47fda03a2d371d46fae666cca1f7fbe00d690fa
Enoferge <jade.escapist@yandex.ru>
2025-06-04 14:53:24 +0300

    sp2 refac: clean code a little

--- src/utils/compile.ts
+++
@@ -1,5 +0,0 @@
-import { compile as hbsCompile } from 'handlebars';
-
-export function compile(template: string, context: Record<string, unknown>) {
-  return hbsCompile(template)(context);
-}
--- src/utils/equal.ts
+++
@@ -1,9 +0,0 @@
-export function areSimpleObjectsEqual(
-  obj1: Record<string, string | undefined>,
-  obj2: Record<string, string | undefined>,
-): boolean {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-  if (keys1.length !== keys2.length) { return false; }
-  return keys1.every((key) => obj1[key] === obj2[key]);
-}
