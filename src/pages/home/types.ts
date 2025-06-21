5992a9b5af41a2c207764f6909612658a9414155
Enoferge <jade.escapist@yandex.ru>
2025-06-21 22:13:23 +0300

    sp3 fix: fix linter errors

--- src/pages/home/types.ts
+++
@@ -1,6 +0,0 @@
-import type { RawPropsWithChildren } from '@/core/block/types';
-import type { PageName } from '@/navigation/types';
-
-export interface HomePageProps extends RawPropsWithChildren {
-  pages?: Array<{ page: PageName; linkText: string }>;
-}
